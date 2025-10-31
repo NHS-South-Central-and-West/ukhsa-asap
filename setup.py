da3ff525fd26000bfa906fc86d75b63668289e6c
Edward Chick <91076016+ed-chick@users.noreply.github.com>
2025-10-31 10:55:17 +0000

    UPDATE: added initial information about the package

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
-# setup
+from setuptools import setup
+
+setup(
+    name='ukhsa-asap',
+    version='0.1.0',
+    description='Import the data that feeds the UKHSA dashboard',
+    author='Specialist Analytics Team - BI Analytics - South, Central and West CSU',
+    author_email='scwcsu.analytics.specialist@nhs.net',
+    packages=['ukhsa-asap'],
+    install_requires=[
+        'polars',
+        'pandas',
+        'urllib3',
+        'json'
+    ],
+)
